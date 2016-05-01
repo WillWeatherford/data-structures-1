d09ee492f3b8e0fe6c83204d4cc639f60436982f
Will Weatherford <weatherford.william@gmail.com>
2016-05-01 16:47:25 -0700

    Using middle of list instead of start of list for better performance in worst-case scenarios for quick-sort.

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -6,7 +6,7 @@ def quick_sort(your_list):
     """Quick sort algorithm using list comprehensions."""
     if not your_list:
         return []
-    pivot = your_list[0]
+    pivot = your_list[len(your_list) // 2]
     smaller = quick_sort([x for x in your_list[1:] if x < pivot])
     larger = quick_sort([x for x in your_list[1:] if x >= pivot])
     return smaller + [pivot] + larger
